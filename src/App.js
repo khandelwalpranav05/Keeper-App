b1da664d955fc95ff3151f275f99fd8f5a33e492
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-24 16:52:04 +0530

    add and delete function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
-import notes from "./notes";
+import CreateArea from "./components/CreateArea";
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        // console.log(note);
+        setNotes(prevState => {
+            return [...prevState, newNote];
+        })
+    }
+
+    function deleteNote(id) {
+        setNotes(prevState => {
+            return prevState.filter((noteItem,index)=>{
+                return index!==id;
+            });
+        });
+    }
+
     return (
         <div className="App">
             <Header/>
-            {notes.map((noteItem)=> {
-                return <Note
-                    key = {noteItem.key}
-                    title={noteItem.title}
-                    content={noteItem.content}
-                />
+            <CreateArea onAdd={addNote}/>
+            {notes.map((noteItem, index) => {
+                return <Note id={index} key={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
             })}
             <Footer/>
         </div>
